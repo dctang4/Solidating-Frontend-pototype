dce7db56679c94a87c336babf6d260cf78e5e0c0
Chun Hin D. Tang <dtang955472@gmail.com>
2022-01-31 17:42:05 -0500

    update

--- solidating/src/components/pages/RegPassions.js
+++ solidating/src/components/pages/RegPassions.js
@@ -1,61 +1,167 @@
-import React, {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './RegPassions.scss';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const RegPassions = ({handleAdd}) => {
-
+const RegPassions = ({ handleAdd }) => {
   const navigate = useNavigate();
 
-  const [passions, setPassions] = useState({passions: []})
+  const [passions, setPassions] = useState({ passions: [] });
 
   const handleClick = (event) => {
-
-    if(passions.passions.filter((passion) => passion === event.target.name).length > 0) {
-      const newPassions = passions.passions.filter((passion) => passion !== event.target.name)
-      setPassions({passions: newPassions})
+    event.preventDefault();
+    if (passions.passions.filter((passion) => passion === event.target.name).length > 0) {
+      const newPassions = passions.passions.filter(
+        (passion) => passion !== event.target.name
+      );
+      setPassions({ passions: [...newPassions] });
     } else {
-      setPassions({passions: [...passions.passions, event.target.name]})
+      setPassions({ passions: [...passions.passions, event.target.name] });
     }
-    
 
-  }
+  };
   const handleContinue = (event) => {
     event.preventDefault();
-    handleAdd(passions)
-    navigate('../photos')
-  }
+    handleAdd(passions);
+    navigate('../photos');
+  };
 
   return (
     <div className='passions-reg'>
-      <h1>Who are you passionsed in?</h1>
-      <section>
-        <FontAwesomeIcon icon={['fas', 'dumbbell']}/> 
-        <br/>
-        <FontAwesomeIcon icon={['fas', 'coffee']}/> 
-        <br/>
-        <FontAwesomeIcon icon={['fas', 'hot-tub']}/> 
-        <br/>
-        <FontAwesomeIcon icon={['fas', 'golf-ball']}/> 
-        <br/>
-        <FontAwesomeIcon icon={['fas', 'graduation-cap']}/> 
-        <br/>
-        <FontAwesomeIcon icon={['fas', 'mobile-alt']}/> 
-        <br/>
-        <FontAwesomeIcon icon={['fas', 'biking']}/> 
-        <br/>
-        <FontAwesomeIcon icon={['fas', 'paint-brush']}/> 
-        <br/>
-        <FontAwesomeIcon icon={['fas', 'plane']}/> 
-        <br/>
-        <FontAwesomeIcon icon={['fas', 'utensils']}/> 
-        <br/>
-      </section>
-      
+      <h1>Who are you passionate in?</h1>
+      <form>
+        <button
+          className='passions-btn'
+          name='exercise'
+          onClick={handleClick}
+          alt='exercise'
+        >
+          <FontAwesomeIcon icon={['fas', 'dumbbell']} />
+        </button>
+        <br />
+
+        <button
+          className='passions-btn'
+          name='coffee'
+          onClick={handleClick}
+          alt='coffee'
+        >
+          <FontAwesomeIcon icon={['fas', 'coffee']} />
+        </button>
+        <br />
+
+        <button
+          className='passions-btn'
+          name='hot tub'
+          onClick={handleClick}
+          alt='hot tub'
+        >
+          <FontAwesomeIcon icon={['fas', 'hot-tub']} />
+        </button>
+        <br />
+
+        <button
+          className='passions-btn'
+          name='golf'
+          onClick={handleClick}
+          alt='golf'
+        >
+          <FontAwesomeIcon icon={['fas', 'golf-ball']} />
+        </button>
+        <br />
+
+        <button
+          className='passions-btn'
+          name='studying'
+          onClick={handleClick}
+          alt='studying'
+        >
+          <FontAwesomeIcon icon={['fas', 'graduation-cap']} />
+        </button>
+        <br />
+
+        <button
+          className='passions-btn'
+          name='swimming'
+          onClick={handleClick}
+          alt='swimming'
+        >
+          <FontAwesomeIcon icon={['fas', 'swimming-pool']} />
+        </button>
+        <br />
+
+        <button
+          className='passions-btn'
+          name='mobile phone'
+          onClick={handleClick}
+          alt='mobile phone'
+        >
+          <FontAwesomeIcon icon={['fas', 'mobile-alt']} />
+        </button>
+        <br />
+
+        <button
+          className='passions-btn'
+          name='driving'
+          onClick={handleClick}
+          alt='driving'
+        >
+          <FontAwesomeIcon icon={['fas', 'car']} />
+        </button>
+        <br />
+
+        <button
+          className='passions-btn'
+          name='biking'
+          onClick={handleClick}
+          alt='biking'
+        >
+          <FontAwesomeIcon icon={['fas', 'biking']} />
+        </button>
+        <br />
+
+        <button
+          className='passions-btn'
+          name='painting'
+          onClick={handleClick}
+          alt='painting'
+        >
+          <FontAwesomeIcon icon={['fas', 'paint-brush']} />
+        </button>
+        <br />
+
+        <button
+          className='passions-btn'
+          name='traveling'
+          onClick={handleClick}
+          alt='traveling'
+        >
+          <FontAwesomeIcon icon={['fas', 'plane']} />
+        </button>
+        <br />
+
+        <button
+          className='passions-btn'
+          name='eating'
+          onClick={handleClick}
+          alt='eating'
+        >
+          <FontAwesomeIcon icon={['fas', 'utensils']} />
+        </button>
+        <br />
+
+        <input 
+          type='submit'
+          className='continue-Bt'
+          value='Continue'
+          onClick={handleContinue}
+          alt='Continue button'
+        />
+
+      </form>
     </div>
-  )
-  
-}
+  );
+};
 
 export default RegPassions;
--- solidating/src/components/pages/RegPhotos.js
+++ solidating/src/components/pages/RegPhotos.js
@@ -4,6 +4,46 @@ import './RegPhotos.scss';
 
 const RegPhotos = ({handleAdd}) => {
 
+  const navigate = useNavigate();
+
+  const [photos, setPhotos] = useState({photos: ''})
+
+  const handleChange = (event) => {
+
+    setPhotos({photos: event.target.value})
+
+  }
+  const handleContinue = (event) => {
+    event.preventDefault();
+    handleAdd(photos)
+    navigate('../mybasics')
+  }
+
+  return (
+    <div className='photos-reg'>
+      {/* <img alt='photos icon'/> */}
+      <h1>What's your photos?</h1>
+      <form onSubmit={handleContinue}>
+        <input
+          type='text'
+          className='loaction'
+          name='photos'
+          placeholder='Enter photos Manually'
+          value={photos.photos}
+          onChange={handleChange}
+          alt='input for your photos'
+        />
+        <input 
+          type='submit'
+          className='continue-Bt'
+          value='Continue'
+          alt='Continue button'
+        />
+      </form>
+      
+    </div>
+  )
+
 }
 
 export default RegPhotos;
